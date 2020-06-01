a680a7f8178c8102df69a8cbf5a2ee78db1a254d
pinkimasud <60094830+pinkimasud@users.noreply.github.com>
2020-06-01 11:17:11 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// owl carousel
+// $(document).ready(function(){
+//     $(".owl-carousel").owlCarousel();
+//   });
+//   animate js
+
+            //   <script>
+            //   new WOW().init();
+            //   </script>
